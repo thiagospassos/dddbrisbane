c9322bcccc89a0f576d55a1f0f5807b8200b64af
Thiago Passos <thiago@passos.com.au>
2017-11-14 22:23:21 +1000

    updating get method to return 3 values

--- DDDBrisbane/Controllers/ValuesController.cs
+++ DDDBrisbane/Controllers/ValuesController.cs
@@ -24,7 +24,7 @@ namespace DDDBrisbane.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
